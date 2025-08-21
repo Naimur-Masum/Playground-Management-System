ae99408bed74fe7f6a05c71102e8b5893d96174a
Masum <masumnaim2@gmail.com>
2025-08-21 16:17:57 +0600

    new code

---
+++ view/login.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head></head>
+    <body>
+        <form action="" method="">
+            <label for="userName">user name</label>
+            <input type="text" id="userName"><br>
+            <label for="pass">Password</label>
+            <input type="password" id="pass"><br>
+            <input type="submit" value="submit">
+
+        </form>
+    </body>
+</html>
