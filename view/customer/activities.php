b9e13e0e21f34da74bc03951bd63a6e7552a5430
Alvi <mdalvi460@gmail.com>
2025-10-10 20:55:09 +0600

    Redoing customer activities for HTML

--- view/customer/activities.php
+++ view/customer/activities.php
@@ -7,35 +7,55 @@ require_once("../../controller/CustomerBookingController.php");
 <head>
     <title>Choose Activity</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&family=Bebas+Neue&display=swap" rel="stylesheet">
+</head>
 </head>
 <body>
     <div class="sidebar">
-        <h2>Customer Panel</h2>
-        <a href="activities.php" style="color:sandybrown">Activities</a>
-        <a href="../logout.php">Logout</a>
+        <nav>
+            <ul>
+                <u><li><a href="activities.php">Make Booking</a></li></u>
+                <li><a href="payment.php">My Payments</a></li>
+                <li><a href="customerDashboard.php">Dashboard</a></li>
+            </ul>
+        </nav>
+
+        <div class="sidebar-bottom">
+            <a href="../logout.php" class="logout-btn">Log out</a>
+        </div>
     </div>
 
-    <div class="main">
-        <h1>Available Activities</h1>
-
+    <div class="content">
         <?php if (isset($_GET['message'])) { ?>
-            <p style="color:green;"><?php echo htmlspecialchars($_GET['message']); ?></p>
+            <p class="success-message"><?php echo htmlspecialchars($_GET['message']); ?></p>
         <?php } ?>
 
         <form method="POST" action="../../controller/CustomerBookingController.php">
-            <label for="activity">Activity:</label>
-            <select name="activity_id" required>
-                <?php foreach ($activities as $act) { ?>
-                    <option value="<?php echo $act['activity_id']; ?>">
-                        <?php echo htmlspecialchars($act['name']) . " - $" . htmlspecialchars($act['price']); ?>
-                    </option>
-                <?php } ?>
-            </select><br><br>
+            <section class="select-activity">
+                <h1>Select Activity</h1>
+                <div class="activity-selection">
+                    <?php foreach ($activities as $act) { ?>
+                        <label class="activity-card">
+                            <input type="radio" name="activity_id" value="<?php echo $act['activity_id']; ?>" required>
+                            <h4><?php echo htmlspecialchars($act['name']); ?></h4>
+                            <p>$<?php echo htmlspecialchars($act['price']); ?></p>
+                        </label>
+                    <?php } ?>
+                </div>
+            </section>
 
-            <label for="booking_date">Booking Date:</label>
-            <input type="date" name="booking_date" required><br><br>
+            <br><br><br>
 
-            <input type="submit" name="book_activity" value="Book Activity">
+            <section class="select-time">
+                <h1>Select Booking Date</h1>
+                <label for="booking_date"></label>
+                <input type="date" name="booking_date" required>
+            </section>
+
+            <br><br><br><br>
+            <input type="submit" name="book_activity" value="Book Activity" class="btn-submit">
         </form>
     </div>
 </body>
